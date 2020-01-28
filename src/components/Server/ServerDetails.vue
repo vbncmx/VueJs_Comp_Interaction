2747dd810904919deee51d07b2a4b147fa38eb8e
vbncmx <vbncmx@mail.ru>
2020-01-29 01:41:45 +0300

    add reset button

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,21 +1,28 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <p v-if="server">Server #{{ server.id}}: {{ server.status }}</p>
-    </div>
-
+  <div class="col-xs-12 col-sm-6" v-if="server">
+    <p>Server #{{ server.id}}: {{ server.status }}</p>
+    <button @click="resetStatus">reset</button>
+  </div>
 </template>
 
 <script>
-import eventBus from "../eventBus"
+import eventBus from "../eventBus";
 
 export default {
   data() {
     return {
       server: null
+    };
+  },
+  methods: {
+    resetStatus() {
+      if (this.server) {
+        this.server.status = "Normal";
+      }
     }
   },
   mounted() {
-    eventBus.$on("server-selected", server => this.server = server)
+    eventBus.$on("server-selected", server => (this.server = server));
   }
-}
+};
 </script>
